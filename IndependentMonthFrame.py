9c868bcc5b2d080417d0b25cc68b28d6fe77d99b
Peixin Liang <peterliangap@outlook.com>
2018-03-26 19:35:24 -0400

    Update action binding after the viewing date change

--- IndependentMonthFrame.py
+++ IndependentMonthFrame.py
@@ -4,12 +4,39 @@ from Clock import *
 
 class IndependentMonthFrame(MonthFrame.MonthFrame):
     def __init__(self, master=None):
+        # Pass a different font size for showing purpose
         super().__init__(month=Clock.get_instance().currentMonth, master=master, font_size=17)
 
+    # Different title
     def place_content(self):
         super().place_content()
         self.monthTitle.config(text=str(Clock.monthTuple[self.showingMonth - 1]) + " " + str(self.showingYear))
 
+    # Different title
     def update_graphics(self):
         super().update_graphics()
         self.monthTitle.config(text=str(Clock.monthTuple[self.showingMonth - 1]) + " " + str(self.showingYear))
+
+    # Need to consider days of next and previous month
+    def update_bind_action_days(self):
+        for weekNum in range(len(self.days)):
+            for day in self.days[weekNum]:
+                # Previous Month
+                if weekNum < 2 and int(day.cget("text")) > 14:
+                    # If it is January
+                    if self.showingMonth == 1:
+                        day.bind("<Button-1>", lambda event, d=day.cget("text"), m=12,
+                                 y=self.showingYear-1: self.dayCallback(d, m, y))
+                    else:
+                        day.bind("<Button-1>", lambda event, d=day.cget("text"), m=self.showingMonth-1,
+                                 y=self.showingYear: self.dayCallback(d, m, y))
+                # Next month
+                elif weekNum > 3 and int(day.cget("text")) < 21:
+                    # If it is December
+                    if self.showingMonth == 12:
+                        day.bind("<Button-1>", lambda event, d=day.cget("text"), m=1,
+                                 y=self.showingYear+1: self.dayCallback(d, m, y))
+                    else:
+                        day.bind("<Button-1>", lambda event, d=day.cget("text"), m=self.showingMonth+1,
+                                 y=self.showingYear: self.dayCallback(d, m, y))
+
--- tkinterTesting.py
+++ tkinterTesting.py
@@ -5,6 +5,8 @@ def do_this(widget, number):
     print(widget)
     print(number)
 
+def do_that(event):
+    print(event)
 
 tk = Tk()
 
@@ -13,6 +15,7 @@ button1.configure(command=lambda btn=button1: do_this(btn, 1))
 button1.pack()
 
 label1 = Label(tk, text="Hello")
+label1.bind("<Button-3>", do_that)
 label1.bind("<Button-1>", lambda z: do_this("The WORLD!", 2))
 label1.pack()
 
