35b6ace791cb3568cf55493d107d0f21d6559e99
Peixin Liang <peterliangap@outlook.com>
2018-03-30 13:22:49 -0400

    tk testing

--- tkinterTesting.py
+++ tkinterTesting.py
@@ -1,22 +1,7 @@
-from tkinter import *
+def make_incrementor(n):
+    return lambda x: x + n
 
 
-def do_this(widget, number):
-    print(widget)
-    print(number)
-
-def do_that(event):
-    print(event)
-
-tk = Tk()
-
-button1 = Button(tk, text="Click")
-button1.configure(command=lambda btn=button1: do_this(btn, 1))
-button1.pack()
-
-label1 = Label(tk, text="Hello")
-label1.bind("<Button-3>", do_that)
-label1.bind("<Button-1>", lambda z: do_this("The WORLD!", 2))
-label1.pack()
-
-tk.mainloop()
+f = make_incrementor(10)
+print(f(1))
+print(f(1))
