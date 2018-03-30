80281109de6c4ba869077b0d99a77976a7c6d5ab
Ninngyou <PeixinLiang@users.noreply.github.com>
2018-03-31 04:56:54 +0800

    Create TodoItemList

--- TODO/frameTest.py
+++ TODO/frameTest.py
@@ -1,1 +1,11 @@
 import tkinter
+from TODO import TodoItem, Event
+
+
+tk = tkinter.Tk()
+todoitem = TodoItem.TodoItem(Event.Event("title","content","10","2018/3/10", "1310", False, True, "2018/3/1"),
+                             master=tk)
+
+todoitem.grid(row=1, column=1)
+
+tk.mainloop()
