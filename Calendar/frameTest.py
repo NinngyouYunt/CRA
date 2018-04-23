4131768199e42dab31597ea00d6b41f34d939ea1
Peixin Liang <peterliangap@outlook.com>
2018-04-23 15:45:08 -0400

    removing test files

--- Calendar/frameTest.py
+++
@@ -1,8 +0,0 @@
-import tkinter as tk
-from Calendar import CalendarFrame
-
-root = tk.Tk(className="FrameTesting")
-root.config(bg="white")
-cf = CalendarFrame.CalendarFrame(root)
-
-root.mainloop()
